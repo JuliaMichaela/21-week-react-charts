e51a3f4cef9eb7c7a43b91cab3022976b5a885af
JuliaMichaela <juliaperez@yandex.ru>
2023-09-19 22:58:35 +0300

    added chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Chart } from "react-google-charts";
+import './style/App.css';
 
 function App() {
+
+  const data = [
+    ["Task", "Hours per Day"],
+    ["Work", 8],
+    ["Eat", 2],
+    ["Training", 1],
+    ["Stady", 2],
+    ["Housework", 4],
+    ["Sleep", 7],
+  ];
+  
+  const options = {
+    title: "My Daily Activities",
+    is3D: true,
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
+
     </div>
   );
 }
 
 export default App;
+
