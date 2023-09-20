ca5a2717e775e922f4dc54ab678b67b5d7c17119
JuliaMichaela <juliaperez@yandex.ru>
2023-09-20 16:06:46 +0300

    Added GeoChart

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import App from './App';
+import { AppGeoChart } from './GeoChart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <AppGeoChart/>
   </React.StrictMode>
 );
